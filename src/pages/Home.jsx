02bc5980ebdfbb298331d8b722f20b046ea9689c
dennisko <dennis0324@naver.com>
2024-05-23 22:13:42 +0900

    remove: custom cursor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,9 @@
-// import { ReactComponent as WorthyLogo } from "../icons/logo.svg";
-// import { Input, Button } from "antd";
 import { ReactComponent as ArrowDown } from "../icons/arrowDown.svg";
 
-import { useState } from "react";
 import Fan from "../components/fan/Fan";
 function Home() {
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
   return (
-    <div
-      className="w-full flex-1 justify-center cursor-none"
-      onMouseMove={(e) => setMousePos({ x: e.clientY, y: e.clientX })}
-    >
-      <div
-        className="fixed w-40 h-40 rounded-full -translate-x-1/2 -translate-y-1/2 grid justify-center content-center"
-        style={{ top: `${mousePos.x}px`, left: `${mousePos.y}px` }}
-      >
-        <div className="bg-secondary w-3 h-3 rounded-full"></div>
-      </div>
-
+    <div className="w-full flex-1 justify-center">
       <div className="h-full w-full overflow-y-auto no-scrollbar relative snap-y">
         <div className="h-full w-full grid justify-center content-center relative snap-start snap-proximity">
           <Fan className="my-3" power={true} />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,6 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
 function Login() {
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
 
@@ -7,10 +10,39 @@ function Login() {
   return (
     <div className="w-full h-full grid items-center">
       <section className="flex flex-col items-center">
-        로그인
-        <input placeholder="ID" className="py-1 px-2 placeholder-gray-200" />
+        <span className="text-4xl my-10">로그인</span>
+
+        <TextField
+          label="ID"
+          variant="outlined"
+          className="w-1/3 min-w-72"
+          sx={{
+            "& label.Mui-focused": {
+              color: "white",
+            },
+            "& .MuiInput-underline:after": {
+              borderBottomColor: "rgba(var(--highlight))",
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "white",
+              },
+              "&:hover fieldset": {
+                borderColor: "white",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "rgba(var(--highlight))",
+              },
+            },
+          }}
+        />
+        <Button variant="contained" className="my-5">
+          로그인
+        </Button>
+
+        <span>다른 방식으로 로그인</span>
         <a onClick={redirect}>login with kakao</a>
-        <a href="/20194043">this is going to redirect to internal server</a>
+        {/* <a href="/20194043">this is going to redirect to internal server</a> */}
       </section>
     </div>
   );
