d0b0b62acab127f809ab09711cba0474b2a2e166
dennisKo <dennis0324@naver.com>
2024-05-24 23:29:37 +0900

    add: register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,38 @@
+function Register() {
+  return (
+    <section className="w-full h-full grid items-center">
+      <div className="flex flex-col items-center"></div>
+      {/* <section className="flex flex-col items-center">
+    <span className="text-4xl my-10 text-primary">로그인</span>
+
+    <TextField
+      label="ID"
+      variant="outlined"
+      className="w-1/5 min-w-72 my-4 transition-all duration-1000"
+      sx={TextFieldSx}
+    />
+    <TextField
+      label="password"
+      variant="outlined"
+      type="password"
+      className="w-1/5 min-w-72 my-4 transition-all duration-1000"
+      style={passwordStyle}
+      sx={TextFieldSx}
+    />
+    <Button
+      variant="contained"
+      className="my-5"
+      onClick={loginHandler}
+      sx={ButtonSx}
+    >
+      {loginBtnText}
+    </Button>
+
+    <span className="text-primary my-4">다른 방식으로 로그인</span>
+    <KakaoLogin />
+  </section> */}
+    </section>
+  );
+}
+
+export default Register;
